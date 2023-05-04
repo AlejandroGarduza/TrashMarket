d5280821411ad63e4a639d540ed2cc3c2d70e878
Alejandro Garduza <alexgm412@gmail.com>
2023-05-04 10:05:26 -0600

    Antes del rediseño

--- JavaScript/checkout.js
+++ JavaScript/checkout.js
@@ -49,7 +49,7 @@ window.addEventListener("DOMContentLoaded", async () => {
       html += `
       <label style="margin-left: 40px; margin-right: 40px; text-align: justify;">${"Cantidad: "+datosTicket.cantidad}</label>
       <label style="margin-left: 40px; margin-right: 40px; text-align: justify;">${"Total: "+datosTicket.total}</label>
-      <label style="margin-left: 40px; margin-right: 40px; text-align: justify;">${"Fecha de Compra: "+fechaFormateada}</label>
+      <label style="margin-left: 40px; margin-right: 40px; text-align: justify;">${"Fecha de Compra: "+new Date(datosTicket.fecha).toLocaleString()}</label>
       <label style="margin-left: 40px; margin-right: 40px; text-align: justify;">${"Referencia de Pago: "+datosTicket.refPago}</label> `;
     });
     ticket.innerHTML = html;
@@ -66,15 +66,9 @@ window.addEventListener("DOMContentLoaded", async () => {
           console.log(doc.data().titulo);
           console.log('Datos del documento:', doc.data());
           nombreP = ""+doc.data().titulo;
+          console.log(nombreP)
           urlIMG = ""+doc.data().url;
-        } else {
-          console.log('No se encontró el documento');
-        }
-      }).catch((error) => {
-        console.error('Error al obtener el documento:', error);
-      });
-
-      html2 += `
+          html2 += `
       <h1 style="margin-left: 40px; margin-top: 5px; font-size: 50px;">Compra Realizada Exitosamente</h1>
       <h3 style="margin-left: 40px; margin-top: 5px;">Resumen del Pedido</h3>
 
@@ -82,12 +76,23 @@ window.addEventListener("DOMContentLoaded", async () => {
 
       
           `;
-
-        nombreProducto.innerHTML = html2;
-
-        //Mostrar imagen
+          nombreProducto.innerHTML = html2;
+          //Mostrar imagen
         let html3 = ` <img src="${urlIMG}" alt="" style="width: 100px; margin-left: 40px; margin-right: 40px; text-align: justify;">`;
         imgProducto.innerHTML = html3;
+        } else {
+          console.log('No se encontró el documento');
+        }
+        
+      }).catch((error) => {
+        console.error('Error al obtener el documento:', error);
+      });
+
+      
+
+        
+
+        
 
 
 });
