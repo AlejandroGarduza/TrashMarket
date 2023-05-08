ec1466187533061d55cc47f539dd0ab294a34924
Antonio Estudillo <jesusestudillo@live.com.mx>
2023-05-08 17:25:14 -0600

    busqueda

--- JavaScript/busquedas.js
+++ JavaScript/busquedas.js
@@ -1,46 +1,73 @@
-import {usuariosRef, where, getDocs, query} from './firebase.js'
+import {usuariosRef, where, getDocs, query, postRef} from './firebase.js'
    
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const searchTerm = urlParams.get('id')
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const searchTerm = urlParams.get('id')
 
-    const q = query(usuariosRef, where("nombre", "==", searchTerm));
-    
-    const busquedas = document.getElementById('resultados-busqueda')
+// Realiza una consulta donde el campo "nombre" contenga la subcadena "searchTerm"
+const q = query(usuariosRef, where("nombre_completo", ">=", searchTerm ), where("nombre_completo", "<=", searchTerm + "\uf8ff"));
+const post = query(postRef, where("titulo", ">=", searchTerm), where("titulo", "<=", searchTerm + "\uf8ff")); 
 
-    getDocs(q).then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
+const busquedas = document.getElementById('resultados-busqueda')
 
+getDocs(q).then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
 
-        const docData = doc.data();
+    const docData = doc.data();
 
-        const publicacionDiv = document.createElement('div');
-		publicacionDiv.classList.add('publicacion')
-        
-		publicacionDiv.innerHTML = `
-        <div class="d-flex flex-wrap align-items-center">
-            <div class="col-md-2">
-                <img class="img-perfil" src="${docData.url}">
-            </div>
-            <div class="col-md-10">
-                <h2>${docData.nombre} ${docData.apellido}</h2>
-                <p>${docData.descripcion}</p>
-            </div>
+    const publicacionDiv = document.createElement('div');
+	publicacionDiv.classList.add('publicacion')
+
+	publicacionDiv.innerHTML = `
+    <div class="d-flex flex-wrap align-items-center">
+        <div class="col-md-2">
+            <img class="img-perfil" src="${docData.url}">
         </div>
+        <div class="col-md-10">
+            <h2>${docData.nombre} ${docData.apellido}</h2>
+            <p>${docData.descripcion}</p>
+        </div>
+    </div>
+	`;
 
-		`;
+    busquedas.appendChild(publicacionDiv)
 
-        busquedas.appendChild(publicacionDiv)
+    publicacionDiv.addEventListener('click', () => {
+		window.location.replace(`vista-otro-usuario.html?id=${docData.email}`);
+	});
+  });
+}).catch((error) => {
+  console.log("Error:", error);
+});
 
-        publicacionDiv.addEventListener('click', () => {
-			window.location.replace(`vista-otro-usuario.html?id=${docData.email}`);
-
-		});
+getDocs(post).then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+  
+      const docData = doc.data();
+  
+      const publicacionDiv = document.createElement('div');
+      publicacionDiv.classList.add('publicacion')
+  
+      publicacionDiv.innerHTML = `
+      <div class="d-flex flex-wrap align-items-center">
+          <div class="col-md-2">
+              <img class="img-perfil" src="${docData.url}">
+          </div>
+          <div class="col-md-10">
+              <h2>${docData.titulo}</h2>
+              <p>${docData.descripcion}</p>
+          </div>
+      </div>
+      `;
+  
+      busquedas.appendChild(publicacionDiv)
+  
+      publicacionDiv.addEventListener('click', () => {
+          window.location.replace(`visualizarPost.html?id=${docData.titulo}`);
       });
-
-      
-
-    }).catch((error) => {
-      console.log("Error:", error);
     });
+  }).catch((error) => {
+    console.log("Error:", error);
+  });
