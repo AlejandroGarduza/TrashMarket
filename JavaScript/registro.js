e84566df7fefbecacbb2878730e0d3f4a36d692b
Antonio Estudillo <jesusestudillo@live.com.mx>
2023-05-25 04:03:40 -0600

    a

--- JavaScript/registro.js
+++ JavaScript/registro.js
@@ -27,35 +27,40 @@ const formulario = document.getElementById('registro-usuario');
         const urlPerfil ='https://static.vecteezy.com/system/resources/previews/014/300/061/non_2x/man-profile-glyph-icon-anonymous-photo-for-documents-illustration-vector.jpg'
 
         if(confirmacion.value === password.value){
-            try{
-                const credenciales = await createUserWithEmailAndPassword(auth, authCorreo, authPassword)
-                console.log(credenciales)
-                saveUsuario(nombre.value, apellido.value, correo.value, tel.value, password.value, calle.value, cp.value,numeroCasa.value, "Escribe tu descripción aquí", urlPerfil)
-                formulario.reset()
-                mostrarMensaje("Bienvenido: " + nombre.value, "success")
-                setTimeout(function() {
+            if (tel.value.length === 10 && /^\d+$/.test(tel.value)) {
+                try {
+                  const credenciales = await createUserWithEmailAndPassword(auth, authCorreo, authPassword);
+                  console.log(credenciales);
+                  saveUsuario(nombre.value, apellido.value, correo.value, tel.value, password.value, calle.value, cp.value, numeroCasa.value, "Escribe tu descripción aquí", urlPerfil);
+                  formulario.reset();
+                  mostrarMensaje("Bienvenido: " + nombre.value, "success");
+                  setTimeout(function() {
                     window.location.replace('../index.html');
-                  }, 2500); 
-               }catch(error){
-                console.log(error.message)
-                console.log(error.code)
-            
-                if(error.code === 'auth/email-already-in-use'){
-                    mostrarMensaje("Correo ya registrado", "error")
+                  }, 2500);
+                } catch (error) {
+                  console.log(error.message);
+                  console.log(error.code);
+          
+                  if (error.code === 'auth/email-already-in-use') {
+                    mostrarMensaje("Correo ya registrado", "error");
+                  }
+                  if (error.code === 'auth/invalid-email') {
+                    mostrarMensaje("Correo no válido", "error");
+                  }
+                  if (error.code === 'auth/weak-password') {
+                    mostrarMensaje("Contraseña demasiado débil", "error");
+                  }
                 }
-                if(error.code === 'auth/invalid-email'){
-                    mostrarMensaje("Correo no valido", "error")
-                }
-                if(error.code === 'auth/weak-password'){
-                    mostrarMensaje("Contraseña demasiado debil", "error")
-                }
-               }  
-        }else{
-            console.log('Las contraseñas nos coinciden')
-            mostrarMensaje("Las contraseñas nos coinciden", "error")
-        }
-    
-        console.log(authCorreo, authPassword);
-    
-        console.log('submiting')
-    })
+              } else {
+                console.log('El número de teléfono debe tener 10 dígitos');
+                mostrarMensaje("El número de teléfono debe tener 10 dígitos", "error");
+              }
+            } else {
+              console.log('Las contraseñas no coinciden');
+              mostrarMensaje("Las contraseñas no coinciden", "error");
+            }
+          
+            console.log(authCorreo, authPassword);
+          
+            console.log('submitting');
+          });
